64ecdf45d074635d8c16a2cda0ce972f9cf42997
MickeyMouse07 <chananet7@gmail.com>
2024-05-25 10:28:56 +1000

    count letter done

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 def main():
     with open("books/frankenstein.txt") as f:
-        file_contents = str(f.read())
+        file_contents = f.read()
         words = []
         words = file_contents.split()
-        print(len(words))
-
+        print(f"{len(words)} words found in the document")
         
+    alpha = {}
+    new = letters(file_contents)
+    for i in new:
+        if i in alpha:
+            alpha[i] += 1
+
+        else:
+            alpha[i] = 1
+
+    print (alpha)
+    
+def accptcha(text):
+    okay = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    newl =""
+    for i in text:
+        if i in okay:
+            newl += i
+    return newl
+
+
+
+def letters(sentence):
+    lowercase = sentence.lower()
+    temp = accptcha(lowercase)
+    return temp
 
 
 main()
