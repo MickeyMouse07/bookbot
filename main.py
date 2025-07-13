f4065d6e454b01e0e7aeba7524af4ad063657239
Chananet <chananet7@gmail.com>
2025-07-13 13:36:42 +1000

    update sys

--- main.py
+++ main.py
@@ -1,61 +1,50 @@
 from stats import get_num_words
+from stats import get_chars_dict
+from stats import chars_dict_to_sorted_list
+import sys
 
 def main():
-    file_path = "books/frankenstein.txt"
-    with open(file_path) as f:
-        file_contents = f.read()
-        read = get_num_words(file_contents)
-        print(f"--- Begin report of {file_path} ---")
-        print(f"{read} words found in the document")
-        print ()
-        print ()
+
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+
+    else:
+        with open(sys.argv[1]) as f:
+            file_contents = f.read()
+            num_words = get_num_words(file_contents)
+            chars_sorted_list = chars_dict_to_sorted_list(get_chars_dict(file_contents))
+            print_report(sys.argv[1], num_words, chars_sorted_list)
         
-    alpha = {}
-    new = letters(file_contents)
-    for i in new:
-        if i in alpha:
-            alpha[i] += 1
-
-        else:
-            alpha[i] = 1
-
-    end_report = chars_dict_to_sorted_list(alpha)
-
-    for pri in end_report:
-        #print (f"The {pri["char"]} character was found {pri["num"]} times")
-        print (f"The {pri['char']} character was found {pri['num']} times")
     
-    print ("--- End report ---")
+        
+        
+        
+        
+  
+
+
+    #book_path = "books/frankenstein.txt"
 
 
 
-def accptcha(text):
-    okay = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    newl =""
-    for i in text:
-        if i in okay:
-            newl += i
-    return newl
 
 
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
 
-def letters(sentence):
-    lowercase = sentence.lower()
-    temp = accptcha(lowercase)
-    return temp
+    print("============= END ===============")
 
 
-def sort_on(dictt):
-    return dictt["num"]
-    
-    
-def chars_dict_to_sorted_list(num_chars_dict):
-    temp_list = []
-    for cha in num_chars_dict:
-        temp_list.append({"char": cha, "num": num_chars_dict[cha]})
-    temp_list.sort(reverse=True, key=sort_on)
-    return temp_list
-    
 
 
 
--- stats.py
+++ stats.py
@@ -1,3 +1,41 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def accptcha(text):
+    okay = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    newl =""
+    for i in text:
+        if i in okay:
+            newl += i
+    return newl
+
+
+
+
+
+def get_chars_dict(text):
+    char = {}
+    lowercase = text.lower()
+    temp = accptcha(lowercase)
+    
+    for i in temp:
+        if i in char:
+            char[i] += 1
+
+        else:
+            char[i] = 1
+    return char
+
+
+def sort_on(dictt):
+    return dictt["num"]
+    
+    
+def chars_dict_to_sorted_list(num_chars_dict):
+    temp_list = []
+    for cha in num_chars_dict:
+        temp_list.append({"char": cha, "num": num_chars_dict[cha]})
+    temp_list.sort(reverse=True, key=sort_on)
+    return temp_list
+    
